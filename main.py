8e4c1ca8088f677e1c98b6908541999d74fa67cf
Scott Ferguson <fergusonsa@yahoo.com>
2020-09-23 09:04:49 -0400

    Got LibreOffice Calc spreadsheet updating with dummy data. Tried with odf and pyexcel modules, but was not successful. Got it working with the ezodf module. Will remove dependencies for odf & pyexcel in future commit. Working scraping from website using requests and BeautifulSoup4, but having issue with login.

--- main.py
+++ main.py
@@ -1,9 +1,73 @@
-# This is a sample Python script.
+import datetime
+import pathlib
+import logging
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+import ods_ezodf
+import python_miscelaneous.configuration as configuration
+
+logger = logging.getLogger(__name__)
 
 
-def fetch_investment_update(name):
-    print(f'Hi, {name}')
+def fetch_investment_update(config):
+    current_values = {"date": datetime.date.today()}
+    login_url = config.get("investment_website").get("login_page_url")
+    login_post_url = config.get("investment_website").get("login_url")
+    username = config.get("investment_website").get("username")
+    password = config.get("investment_website").get("password")
+
+    # Log into the website
+    # Using example https://linuxhint.com/logging_into_websites_python/
+    with requests.Session() as session:
+        # Get the login page
+
+        res = session.get(login_url, verify=False)
+        # Get the _RequestVerificationToken hidden field value
+        bs_content = bs(res.content, "html.parser")
+        token = bs_content.find("input", {"name": "_RequestVerificationToken"})
+
+        login_data = {'Login': username,
+                'Password': password,
+                'RememberLogin': False,
+                '_RequestVerificationToken': token}
+        res = session.post(login_post_url, login_data)
+        home_page = bs(res.content, "html.parser")
+        # Get the summary page
+        portfolio_url = config["investment_website"]["portfolio_url"]
+        res = session.get(portfolio_url)
+        portfolio_content = bs(res.content, "html.parser")
+
+        accounts_config = config.get('accounts')
+        for account_name in accounts_config.keys():
+            value_location = accounts_config[account_name]['website_location']
+            account_id = accounts_config[account_name]['account']
+            account_anchor = portfolio_content.find('a', string=account_id)
+            if value_location == 'portfolio_url':
+                # Get the value from the portfolio summary page
+                # Find the row with the account in the first column
+                row = account_anchor.findParent('tr')
+
+
+
+                value = 10000.00
+            else:
+                # Get the details page the account that contains the required value
+                # Find the row with the account in the first column
+                # Get the value from the page
+                value = 1111.11
+            current_values[account_name] = value
+
+    return current_values
 
 
 if __name__ == '__main__':
-    fetch_investment_update('PyCharm')
+    config = configuration.get_configuration('investment_tracking',
+                                             pathlib.Path('~/investment_tracking'),
+                                             configuration.CONFIGURATION_TYPE_JSON)
+    current_values = fetch_investment_update(config)
+    import pprint
+    pprint.pprint(current_values)
+    # if current_values:
+    #     ods_ezodf.save_investment_update(config, current_values)
---
+++ ods_ezodf.py
@@ -0,0 +1,46 @@
+import pathlib
+import logging
+
+import ezodf
+
+logger = logging.getLogger(__name__)
+
+
+def save_investment_update(config, current_values):
+
+    spreadsheet_config = config.get('spreadsheet')
+    file_path = pathlib.Path(spreadsheet_config.get("path"))
+    if not file_path.is_file():
+        logger.warning('The spreadsheet path {} is not a valid file.'.format(file_path))
+        return
+
+    sheet_name = spreadsheet_config.get("sheet_name")
+    row_index = spreadsheet_config.get('listing_start_row')
+    date_column_index = spreadsheet_config.get('date_column')
+    total_column_index = spreadsheet_config.get('total_column')
+    spreadsheet = ezodf.opendoc(str(file_path))
+    sheet = spreadsheet.sheets[sheet_name]
+    # find the first row in the range that has no date in the first cell
+    while sheet[f'{date_column_index}{row_index}'].value_type == 'date' and row_index < 366:
+        row_index += 1
+    # logger.debug('found empty row or last row at row index: {}'.format(row_index))
+    accounts_config = config.get('accounts')
+    total_value = 0.0
+    for column in current_values.keys():
+        if column == 'date':
+            cell_index = f'{date_column_index}{row_index}'
+            cell = sheet[cell_index]
+            cell.set_value(current_values[column], 'date')
+        else:
+            column_index = accounts_config[column]['column']
+
+            cell = sheet[f'{column_index}{row_index}']
+            cell.set_value(current_values[column], currency='CAD')
+            total_value = total_value + current_values[column]
+
+    cell = sheet[f'{total_column_index}{row_index}']
+    cell.set_value(total_value, currency='CAD')
+    # pprint.pprint(spreadsheet)
+
+    # After row data has been added, save the data
+    spreadsheet.save()
